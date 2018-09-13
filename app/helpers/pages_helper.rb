83570faeb18f908a22ffef22c750de42c619fa14
alexckua <alex@net.ck.ua>
2018-09-14 00:07:18 +0300

    Render content to show post page

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
 module PagesHelper
-  def posted_date(post)
-    l(post.created_at, format: :created_post)
+  def posted_date(datetime)
+    l(datetime, format: :created_date)
   end
 
-  def show_image(image)
+  def posted_time(datetime)
+    l(datetime, format: :created_time)
+  end
+
+  def show_image(image, style)
     image_url = image.url || 'http://placehold.it/750x300'
-    image_tag(image_url, class: 'card-img-top')
+    image_tag(image_url, class: style)
   end
 end
